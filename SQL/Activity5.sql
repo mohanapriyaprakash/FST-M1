21b905180e58b01e84a528403c0071ed8a12cfb0
mohanapriyaprakash <102228925+mohanapriyaprakash@users.noreply.github.com>
2022-04-13 19:02:38 +0530

    Delete Activity5.sql

--- SQL/Activity5.sql
+++
@@ -1,5 +0,0 @@
-UPDATE salesman set grade=200 where salesman_city='Rome';
-
-UPDATE salesman set grade=300 where salesman_name='James Hoog';
-
-UPDATE salesman set salesman_name='Pierre' where salesman_name='McLyon';
