540f467ed03d02735ef7cdf6275e2c1d7bce54a4
mohanapriyaprakash <102228925+mohanapriyaprakash@users.noreply.github.com>
2022-04-13 19:02:17 +0530

    Delete Activity3.sql

--- SQL/Activity3.sql
+++
@@ -1,6 +0,0 @@
----Activity3---
-Select salesman_id,salesman_city from salesman;
-
-SELECT * from salesman where salesman_city='Paris';
-
-SELECT salesman_id,commission from salesman where salesman_name='Paul Adam';
